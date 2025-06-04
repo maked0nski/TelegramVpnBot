3fc6bcf9674033e38c6f282a9a4c1e7f7ac308ad
Oleksandr <maked0nskihtc@gmail.com>
2025-06-04 22:36:55 +0100

    init: Telegram bot initial version

---
+++ src/main/java/com/freedomua/vpn/freedomuavpnbot/config/TelegramBotConfig.java
@@ -0,0 +1,16 @@
+package com.freedomua.vpn.freedomuavpnbot.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "bot")
+public class TelegramBotConfig {
+    @Getter
+    @Setter
+    private String token;
+    private String username;
+}

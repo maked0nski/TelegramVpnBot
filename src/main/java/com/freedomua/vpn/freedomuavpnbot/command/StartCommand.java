0b4c34a3657a80adfc04db243221d5de06b71e51
Oleksandr <maked0nskihtc@gmail.com>
2025-06-23 23:08:19 +0100

    Asynchronous messaging, and localization improvements

--- src/main/java/com/freedomua/vpn/freedomuavpnbot/command/StartCommand.java
+++ src/main/java/com/freedomua/vpn/freedomuavpnbot/command/StartCommand.java
@@ -3,17 +3,14 @@ package com.freedomua.vpn.freedomuavpnbot.command;
 import com.freedomua.vpn.freedomuavpnbot.handler.CommandHandler;
 import com.freedomua.vpn.freedomuavpnbot.model.UserEntity;
 import com.freedomua.vpn.freedomuavpnbot.service.AsyncBotMessageService;
-import com.freedomua.vpn.freedomuavpnbot.service.BotMessageService;
 import com.freedomua.vpn.freedomuavpnbot.service.LocaleService;
 import com.freedomua.vpn.freedomuavpnbot.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.Locale;
 
 @Slf4j
 @Component
